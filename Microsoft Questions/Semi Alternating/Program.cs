d0bdd0080e8260cb641b30cdb8f154e6f421c5b5
Padmini Kota <padmini.ksn@gmail.com>
2021-04-18 23:16:52 -0700

    commit

--- Microsoft Questions/Semi Alternating/Program.cs
+++ Microsoft Questions/Semi Alternating/Program.cs
@@ -42,37 +42,59 @@ The first four letters of S create a semi-alternating substring.
          * ***************************/
         static void Main(string[] args)
         {
-            semiSubstring("babba");
+            semiSubstring("baaabbabbb");
         }
         public static int semiSubstring(String s)
         {
-           //baaabbabbb
-            int i = 0, j = 0, max_len = int.MinValue, a_count = 0, b_count = 0;
-            char[] array = s.ToCharArray();
-            while(i<s.Length)
+            int max_count = 3;
+            if (s.Length < max_count)
+                return s.Length;
+            int count = 1, result = 1;
+            for(int i=1;i<s.Length-1;i++)
             {
-                //char c = array[i];
-                j = i + 1;
-                while(j<array.Length && a_count<1 && b_count<1)
+                if((s[i-1]==s[i])&&(s[i]==s[i+1]))
                 {
-                    if(j<array.Length-1 && array[j]=='a')
-                    {
-                        if ((array[j - 1] == array[j]) && (array[j] == array[j + 1]))
-                            a_count++;
-                    }
-                    else if(j<array.Length-1 && array[j]=='b')
-                    {
-                        if ((array[j - 1] == array[j]) && (array[j] == array[j + 1]))
-                            b_count++;
-                    }
-                    j++;
+                    result = Math.Max(result, count);
+                    count = 1;
                 }
-                max_len = Math.Max(max_len,(j-i));
-                i++;
-                a_count = 0;
-                b_count = 0;
+                else
+                {
+                    count++;
+                }
             }
-            return max_len;
+
+            return Math.Max(result, count + 1); ;
+     
         }
+        //public static int semiSubstring(String s)
+        //{
+        //   //baaabbabbb
+        //    int i = 0, j = 0, max_len = int.MinValue, a_count = 0, b_count = 0;
+        //    char[] array = s.ToCharArray();
+        //    while(i<s.Length)
+        //    {
+        //        //char c = array[i];
+        //        j = i + 1;
+        //        while(j<array.Length && a_count<1 && b_count<1)
+        //        {
+        //            if(j<array.Length-1 && array[j]=='a')
+        //            {
+        //                if ((array[j - 1] == array[j]) && (array[j] == array[j + 1]))
+        //                    a_count++;
+        //            }
+        //            else if(j<array.Length-1 && array[j]=='b')
+        //            {
+        //                if ((array[j - 1] == array[j]) && (array[j] == array[j + 1]))
+        //                    b_count++;
+        //            }
+        //            j++;
+        //        }
+        //        max_len = Math.Max(max_len,(j-i));
+        //        i = j - 1;
+        //        a_count = 0;
+        //        b_count = 0;
+        //    }
+        //    return max_len;
+        //}
     }
 }
